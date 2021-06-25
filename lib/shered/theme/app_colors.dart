840027c9271d723723ff32a65fdef72613fd17a0
Lporchat <pegasusryuseiken2.0@gmail.com>
2021-06-25 09:27:46 -0300

    Melhorias na tela de login

---
+++ lib/shered/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final primary = Color.fromARGB(255, 89, 117, 181);
+  static final white = Color.fromARGB(255, 255, 255, 255);
+}
--- lib/telas/loginScream.dart
+++ lib/telas/loginScream.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:leite/shered/theme/app_colors.dart';
 import 'package:leite/telas/homeScream.dart';
 
 class loginScream extends StatefulWidget {
@@ -15,28 +16,162 @@ class _loginScreamState extends State<loginScream> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          color: Color.fromARGB(255, 89, 117, 181),
-          child: SingleChildScrollView(
+        body: Stack(
+      children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            color: Colors.white,
+            height: MediaQuery.of(context).size.height * 0.6,
+            width: MediaQuery.of(context).size.width,
             child: Column(
-              mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                      top: 90, bottom: 0, left: 0, right: 0),
-                  child: new Image.asset(
-                    'assets/sigpaa.png',
-                    fit: BoxFit.cover,
-                  ),
+                Image(image: AssetImage('assets/sigpaa.png')),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            decoration: BoxDecoration(
+              color: AppColors.primary,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0),
+              ),
+            ),
+            //
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.423,
+              decoration: BoxDecoration(
+                color: AppColors.primary,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      top: 70, bottom: 0, left: 0, right: 0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.458,
+              ),
+              //
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 30, bottom: 0, left: 10, right: 10),
+                    child: TextFormField(
+                        controller: login,
+                        obscureText: false,
+                        // mudança de cor do input
+                        style: TextStyle(color: AppColors.white),
+                        decoration: InputDecoration(
+                            fillColor: Colors.black,
+                            hintText: "Matricula",
+                            hintStyle: TextStyle(color: AppColors.white),
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: AppColors.white,
+                            ),
+                            //borda selecionada
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: BorderSide(
+                                color: AppColors.white,
+                                width: 2.0,
+                              ),
+                            ),
+                            //borda sem esta selecionada
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: BorderSide(
+                                color: AppColors.white,
+                                width: 2.0,
+                              ),
+                            ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 10, bottom: 0, left: 10, right: 10),
+                    child: TextFormField(
+                        controller: senha,
+                        obscureText: true,
+                        style: TextStyle(color: AppColors.white),
+                        decoration: InputDecoration(
+                            hintText: "Senha",
+                            hintStyle: TextStyle(color: AppColors.white),
+                            prefixIcon: Icon(
+                              Icons.vpn_key,
+                              color: AppColors.white,
+                            ),
+
+                            //borda selecionada
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: BorderSide(
+                                color: AppColors.white,
+                                width: 2.0,
+                              ),
+                            ),
+                            //borda sem esta selecionada
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: BorderSide(
+                                color: AppColors.white,
+                                width: 2.0,
+                              ),
+                            ))),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(
+                          top: 30, bottom: 0, left: 10, right: 10),
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints.tightFor(
+                            width: double.infinity, height: 60),
+                        child: ElevatedButton(
+                          child: Text("login", style: TextStyle(fontSize: 25)),
+                          style: ElevatedButton.styleFrom(
+                            //cor do botão
+                            primary: AppColors.white,
+                            //cor do texto
+                            onPrimary: AppColors.primary,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                          onPressed: () {
+                            print(login.text);
+                            print(senha.text);
+                            if (login.text == "Admin" &&
+                                senha.text == "admin") {
+                              print("Deu certo, acessou");
+                              //para puxar a proxima tela e dechar a primeira e retirar o icone
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomeScream()));
+                            } else {
+                              print("Cadastro errado");
+                            }
+                          },
+                        ),
+                      )),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
+
+/*Container(
+                    height: MediaQuery.of(context).size.height * 0.423,
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
@@ -119,7 +254,9 @@ class _loginScreamState extends State<loginScream> {
                               constraints: BoxConstraints.tightFor(
                                   width: 300, height: 55),
                               child: ElevatedButton(
-                                child: Text("login"),
+                                child: Text(
+                                  "login",
+                                ),
                                 style: ElevatedButton.styleFrom(
                                   //cor do botão
                                   primary: Color.fromARGB(255, 89, 117, 181),
@@ -150,10 +287,4 @@ class _loginScreamState extends State<loginScream> {
                       ],
                     ),
                   ),
-                )
-              ],
-            ),
-          )),
-    );
-  }
-}
+*/
